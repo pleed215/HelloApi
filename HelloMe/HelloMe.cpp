b341881d5c3dfb8277f75da63f4d527c1dccd80e
unknown <pleed@naver.com>
2022-04-09 22:55:49 +0900

    Main logic done!

--- HelloMe/HelloMe.cpp
+++ HelloMe/HelloMe.cpp
@@ -1,36 +1,66 @@
 #include <windows.h>
 #include <math.h>
+#include <time.h>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK ChildWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 HINSTANCE g_hInst;
+HWND g_hMain=(HWND)NULL;
 LPCTSTR lpszClass = L"HelloAPI";
 LPCTSTR ChildClassName = L"ChildWin";
 
 
-#define IDC_BUTTON 1001
+#define ID_BTN_START 1001
+#define ID_BTN_HIGHSCORE 1002
+#define IDT_TIMER1 1003
+
 #define BOARD_SIZE 60
 #define DIR_E 1
 #define DIR_S 2
 #define DIR_W 3
 #define DIR_N 4
 #define RESULT_OK 1
-#define RESULT_FEED 2
-#define UPDATE_WALL 3
-#define CELL_SIZE 20
+#define RESULT_FOOD 2
+#define RESULT_WALL 3
+#define RESULT_SELF 4
+#define CELL_SIZE 10
+#define INIT_LENGTH 3
+#define GAME_STATE_STOP 0
+#define GAME_STATE_PLAYING 1
+#define GAME_STATE_END 2
 
+typedef struct {
+	int x, y;
+} Position;
+
+const int MAX_SCORE = BOARD_SIZE * 3;
 int gameBoard[BOARD_SIZE][BOARD_SIZE];
+Position snakeData[MAX_SCORE+3];
+Position foodData;
 int score = 0;
 int length = 3;
 int speed = 1;
 int direction = DIR_E;
-int cellSIze = CELL_SIZE;
+int gameState = GAME_STATE_STOP;
+const int CONTENT_HEIGHT = 100;
+const int WINDOW_WIDTH = CELL_SIZE * BOARD_SIZE + 40;
+const int WINDOW_HEIGHT = CELL_SIZE * BOARD_SIZE + CONTENT_HEIGHT + 100;
+const int START_X = 10;
+const int START_Y = CONTENT_HEIGHT;
+
 void initBoard();
 void gameLoop();
-void renderBoard();
+void renderBoard(HDC);
 int updateBoard();
 void changeDir(int dir);
+void generateFood();
+void gainFood();
+BOOL isFoodPositionOK();
+void drawCell(HDC, Position&);
+
+// Utils
+int rangeRandom(int begin, int end); // end는 포함안함.
 
 
 
@@ -60,13 +90,14 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpszCmdP
 	hWnd = CreateWindow(lpszClass,			//윈도우클래스 이름
 		L"컴퓨터과학과 2학년",			    //윈도우타이틀
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE,   //윈도우스타일
-		200, 200,							//윈도우가 보일때 X Y좌표
-		600, 600,							//윈도우의 폭과 높이				
+		20, 20,							//윈도우가 보일때 X Y좌표
+		WINDOW_WIDTH, WINDOW_HEIGHT,							//윈도우의 폭과 높이				
 		(HWND)NULL,							//부모윈도우 핸들
 		(HMENU)NULL,						//윈도우가 가지는 메뉴핸들
 		hInstance,							//인스턴스핸들
 		NULL);								//여분의 데이터
 
+	g_hMain = hWnd;
 	ShowWindow(hWnd, nCmdShow);
 
 	while (GetMessage(&Message, 0, 0, 0)) {
@@ -85,7 +116,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage,
 	{
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hWnd, &ps);
-		TextOut(hdc, 100, 100, text, lstrlen(text));
+		TextOut(hdc, 10, 10, text, lstrlen(text));
+		renderBoard(hdc);
 		EndPaint(hWnd, &ps);
 		return 0;
 	}
@@ -97,35 +129,92 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage,
 			WS_OVERLAPPEDWINDOW | WS_CHILD,  // 윈도우  스타일 
 			150,       		// 윈도우 보일 때 x 좌표 
 			150,       		// 윈도우 보일 때 y 좌표 
-			260,       		// 윈도우 폭
-			200,       		// 윈도우 높이
+			600,       		// 윈도우 폭
+			600,       		// 윈도우 높이
 			hWnd,         		// 부모 윈도우
 			(HMENU)1000,        	// 차일드 윈도우ID 
 			g_hInst,           		// 인스턴스 핸들 
 			(LPVOID)NULL);      	// 여분의 데이터
 
 		if (!hChildWnd) 	return -1;
-
-		HWND hButton = CreateWindow(
+		CreateWindow(
 			L"button",
-			L"지역대학",
+			L"게임 시작",
 			WS_CHILD | WS_VISIBLE,
-			20, 400, 100, 30,
+			210, 10, 100, 30,
 			hWnd,
-			(HMENU)IDC_BUTTON,
+			(HMENU)ID_BTN_START,
+			g_hInst,
+			(LPVOID)NULL);
+		CreateWindow(
+			L"button",
+			L"하이스코어",
+			WS_CHILD | WS_VISIBLE,
+			320, 10, 100, 30,
+			hWnd,
+			(HMENU)ID_BTN_HIGHSCORE,
 			g_hInst,
 			(LPVOID)NULL);
 
-		ShowWindow(hChildWnd, SW_SHOW);
+		long seed = time(NULL);
+		srand(seed);
+		//ShowWindow(hChildWnd, SW_SHOW);
 		initBoard();
 		return 0;
 	}
 	case WM_COMMAND:
-		if (LOWORD(wParam) == IDC_BUTTON) {
-			MessageBox(hWnd, L"지역대학 버튼이 클릭되었다", L"지역대학", MB_OK | MB_ICONINFORMATION);
+		if (LOWORD(wParam) == ID_BTN_START) {
+			initBoard();
+			gameState = GAME_STATE_PLAYING;
+			SetFocus(hWnd);
+			SetTimer(hWnd, IDT_TIMER1, 100, (TIMERPROC)NULL);
+		}
+		return 0;
+
+	case WM_KEYDOWN:
+		if (gameState == GAME_STATE_PLAYING) {
+			switch (wParam) {
+			case VK_UP:
+				changeDir(DIR_N);
+				break;
+			case VK_DOWN:
+				changeDir(DIR_S);
+				break;
+			case VK_LEFT:
+				changeDir(DIR_W);
+				break;
+			case VK_RIGHT:
+				changeDir(DIR_E);
+				break;
+			case VK_SPACE:
+				gainFood();
+				break;
+			default:
+				break;
+			}
+		}
+		return 0;
+	case WM_TIMER:
+		switch (wParam) {
+			case IDT_TIMER1:
+				if (gameState == GAME_STATE_PLAYING) {
+					int result = updateBoard();
+					if (result == RESULT_WALL || result == RESULT_SELF) {
+						KillTimer(hWnd, IDT_TIMER1);
+						gameState = GAME_STATE_END;
+						MessageBox(hWnd, L"충돌감지", L"게임종료", MB_OK | MB_ICONERROR);
+					}
+					else if (result == RESULT_FOOD) {
+						gainFood();
+					}
+				}
+				
+				break;
+
 		}
 		return 0;
 	case WM_DESTROY:
+		KillTimer(hWnd, IDT_TIMER1);
 		PostQuitMessage(0);
 		return 0;
 	}
@@ -160,6 +249,132 @@ void initBoard() {
 			gameBoard[i][j] = 0;
 		}
 	}
+	score = 0;
+	gameState = GAME_STATE_STOP;
+	direction = DIR_E;
 	int boardCenter = floor(BOARD_SIZE / 2);
+	for (int i = 0; i < INIT_LENGTH; i++) {
+		snakeData[i].x = boardCenter - i;
+		snakeData[i].y = boardCenter;
+	}
+	generateFood();
 }
 
+void renderBoard(HDC hdc) {
+	HPEN blackPen = (HPEN)GetStockObject(BLACK_PEN);
+	HBRUSH blackBrush = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	HBRUSH redBrush = (HBRUSH)CreateSolidBrush(RGB(255, 0, 0));
+	HBRUSH foodBrush = (HBRUSH)CreateSolidBrush(RGB(0, 255, 0));
+	HGDIOBJ oldObj =  SelectObject(hdc, (HGDIOBJ)blackPen);
+
+	int boardLength = BOARD_SIZE * CELL_SIZE;
+	Rectangle(hdc, START_X, START_Y, START_X+boardLength, START_Y+boardLength);
+	SelectObject(hdc, oldObj);
+	
+	for (int i = 0; i < score + INIT_LENGTH; i++) {
+		if (i == 0) {
+			oldObj = SelectObject(hdc, (HGDIOBJ)redBrush);
+		}
+		else {
+			oldObj = SelectObject(hdc, (HGDIOBJ)blackBrush);
+		}
+		drawCell(hdc, snakeData[i]);
+	}
+	SelectObject(hdc, oldObj);
+
+	oldObj = SelectObject(hdc, (HGDIOBJ)foodBrush);
+	drawCell(hdc, foodData);
+	SelectObject(hdc, oldObj);
+
+	DeleteObject((HGDIOBJ)redBrush);
+	DeleteObject((HGDIOBJ)foodBrush);
+}
+
+void drawCell(HDC hdc, Position& p) {
+	int x = START_X + p.x * CELL_SIZE;
+	int y = START_Y + p.y * CELL_SIZE;
+	Rectangle(hdc, x, y, x + CELL_SIZE, y + CELL_SIZE);
+}
+
+int updateBoard() {
+	RECT boardRect = {
+		START_X,
+		START_Y,
+		START_X + CELL_SIZE * BOARD_SIZE,
+		START_Y + CELL_SIZE * BOARD_SIZE,
+	};
+
+	for (int i = score+INIT_LENGTH; i > 0; i--) {
+		snakeData[i] = snakeData[i - 1];
+	}
+	switch (direction) {
+		case DIR_E:
+			snakeData[0].x += 1;
+			break;
+		case DIR_S:
+			snakeData[0].y += 1;
+			break;
+		case DIR_W:
+			snakeData[0].x -= 1;
+			break;
+		case DIR_N:
+			snakeData[0].y -= 1;
+		default:
+			break;
+	}
+
+	if (snakeData[0].x >= BOARD_SIZE || snakeData[0].x < 0 || snakeData[0].y >= BOARD_SIZE || snakeData[0].y < 0) {
+		return RESULT_WALL;
+	}
+
+	for (int i = 0; i < score + INIT_LENGTH - 1; i++) {
+		for (int j = i + 1; j < score + INIT_LENGTH; j++) {
+			if (snakeData[i].x == snakeData[j].x && snakeData[i].y == snakeData[j].y) {
+				return RESULT_SELF;
+			}
+		}
+	}
+
+	if (snakeData[0].x == foodData.x && snakeData[0].y == foodData.y) {
+		return RESULT_FOOD;
+	}
+
+	InvalidateRect(g_hMain, &boardRect, TRUE);
+	return RESULT_OK;
+}
+
+void generateFood() {
+
+	do {
+		foodData.x = rangeRandom(0, BOARD_SIZE);
+		foodData.y = rangeRandom(0, BOARD_SIZE);
+	} while (!isFoodPositionOK());
+}
+
+void gainFood() {
+	generateFood();
+	score += 1;
+}
+
+BOOL isFoodPositionOK() {
+	for (int i = 0; i < score + INIT_LENGTH; i++) {
+		if (snakeData[i].x == foodData.x && snakeData[i].y == foodData.y) {
+			return FALSE;
+		}
+	}
+	return TRUE;
+}
+
+void changeDir(int dir) {
+	if ((direction == DIR_E && dir == DIR_W) ||
+		(direction == DIR_W && dir == DIR_E) ||
+		(direction == DIR_S && dir == DIR_N) ||
+		(direction == DIR_N && dir == DIR_S)) {
+		return;
+	} 
+	direction = dir;
+}
+
+int rangeRandom(int begin, int end) {
+	return begin + (int)floor(rand() % (end-begin-1));
+}
